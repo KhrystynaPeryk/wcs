d927c7df76605aa3059a88629e9c5c4d265c8046
Khrystyna Peryk <kristik991@gmail.com>
2024-04-01 14:39:23 +0400

    width less 875px arrow down visible + onclick div

--- app/components/service-section/service.js
+++ app/components/service-section/service.js
@@ -1,26 +1,31 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './service.module.css'
-import Image from "next/image";
-import servicesImg from '@/public/assets/img/servicesImg.jpg'
 import ServiceItem from './serviceItem';
 
 const Service = () => {
+  const [visibleItem, setVisibleItem] = useState(null);
+
+  const toggleVisibility = (itemName) => {
+    // Toggle logic: if the clicked item is already visible, hide it, otherwise show it
+    setVisibleItem(visibleItem === itemName ? null : itemName);
+  };
+
   return (
     <div className={styles.serviceContainer}>
       <div className={styles.transitional}></div>
-      {/* <Image src={servicesImg} alt='img' className={styles.img}/> */}
       <div className={styles.servicesDesc}>
         <p>Our services</p>
         <div className={styles.itemsContainer}>
-          <ServiceItem serviceName={'Design'} serviceDesc={'Responsive custom design for desktop, tablet and mobile devices.'} />
-          <ServiceItem serviceName={'Front-end Development'} serviceDesc={'We develop UI of your website or web application as per design approved by you.'} />
-          <ServiceItem serviceName={'Back-end Development'} serviceDesc={'Integration / connection of your website to the 3rd party apps with API..'} />
-          <ServiceItem serviceName={'Support'} serviceDesc={'Customer support services for your commercial website.'} />
-          <ServiceItem serviceName={'Cloud Consulting'} serviceDesc={'Optimization of the performance of your website.'} />
+          <ServiceItem serviceName={'Design'} serviceDesc={'Responsive custom design for desktop, tablet and mobile devices.'} toggleVisibility={toggleVisibility} isVisible={visibleItem === 'Design'} />
+          <ServiceItem serviceName={'Front-end Development'} serviceDesc={'We develop UI of your website or web application as per design approved by you.'} toggleVisibility={toggleVisibility} isVisible={visibleItem === 'Front-end Development'} />
+          <ServiceItem serviceName={'Back-end Development'} serviceDesc={'Integration / connection of your website to the 3rd party apps with API.'} toggleVisibility={toggleVisibility} isVisible={visibleItem === 'Back-end Development'} />
+          <ServiceItem serviceName={'Support'} serviceDesc={'Customer support services for your commercial website.'} toggleVisibility={toggleVisibility} isVisible={visibleItem === 'Support'} />
+          <ServiceItem serviceName={'Cloud Consulting'} serviceDesc={'Optimization of the performance of your website.'} toggleVisibility={toggleVisibility} isVisible={visibleItem === 'Cloud Consulting'} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Service
--- app/components/service-section/serviceItem.js
+++ app/components/service-section/serviceItem.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import styles from './serviceItem.module.css'
+'use client'
+import React from 'react';
+import styles from './serviceItem.module.css';
 
-const ServiceItem = ({serviceName, serviceDesc}) => {
+const ServiceItem = ({ serviceName, serviceDesc, toggleVisibility, isVisible }) => {
+    const handleClick = () => {
+        if (window.innerWidth <= 875) {
+            toggleVisibility(serviceName);
+        }
+    };
+
     return (
-        <div className={styles.item}>
+        <div className={styles.item} onClick={handleClick}>
             <h2>{serviceName}</h2>
-            <div>{serviceDesc}</div>
+            <div className={isVisible ? styles.visible : styles.hidden}>{serviceDesc}</div>
         </div>
-    )
-}
+    );
+};
 
-export default ServiceItem
+export default ServiceItem;
