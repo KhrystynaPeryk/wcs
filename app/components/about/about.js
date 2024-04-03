beda309543cc6f57fb978d609ee1680e31683335
Khrystyna Peryk <kristik991@gmail.com>
2024-04-03 13:51:26 +0400

    draft of about component

--- app/components/about/about.js
+++ app/components/about/about.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import styles from './about.module.css'
+import Image from 'next/image'
+import Team from '../../../public/assets/img/about.png'
 
 const About = () => {
     return (
-        <div className={styles.aboutContainer}>about</div>
+        <div className={styles.aboutContainer}>
+            <div className={styles.imgWrapper} >
+                <Image src={Team} alt='about us' fill className={styles.img} />
+            </div>
+            <div className={styles.textWrapper} >
+                <div>
+                    <h1>About us</h1>
+                </div>
+                <div className={styles.text}>
+                    <p>
+                        <b>Web Craft Squad</b> is a start up company of enthusiastic IT specialists who specialize in providing end-to-end solutions for your web application needs. 
+                    </p>
+                    <p>
+                        Our commitment to 'Comprehensive Web Application Development and Support' means that we're here to guide you through every step of your project's journey.
+                    </p>
+                    <p>
+                        From conceptualization to deployment and beyond, our team of experts is dedicated to crafting web applications that not only meet your unique requirements but also exceed your expectations.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
