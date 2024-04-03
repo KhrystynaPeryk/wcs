4b56e62b02df046f7c93094d580dcc50b3732f74
Khrystyna Peryk <kristik991@gmail.com>
2024-04-03 15:29:26 +0400

    draft of footer

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Logo from '../header/logo'
 
 const Footer = () => {
     return (
-        <div>footer</div>
+        <div className={styles.footerContainer}>
+            <div>
+                Transform <b>your vision</b> into a dynamic and effective online presence with us!
+            </div>
+            <Logo color={'#fff'}/>
+        </div>
     )
 }
 
